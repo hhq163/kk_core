674925729aed852d5c9864d2ecad17370de5eee9
hhq163 <hhq163@gmail.com>
2020-04-07 12:06:10 +0800

    change note

--- util/workpool.go
+++ util/workpool.go
@@ -7,7 +7,7 @@ import (
 type Worker func()
 
 type WorkPool struct {
-	work chan Worker //通道，发送worker
+	work chan Worker
 	wg   sync.WaitGroup
 }
 
@@ -18,18 +18,18 @@ func NewWorkPool(maxGoroutines int) *WorkPool {
 	p.wg.Add(maxGoroutines)
 	for i := 0; i < maxGoroutines; i++ {
 		go func() {
-			for w := range p.work { //这种方式从一个chan中取东西， select 是从多个chan中取东西，如果没有则阻塞，只有在通道被关闭时才会终止循环
-				w() //执行
+			for w := range p.work {
+				w()
 			}
 			p.wg.Done()
-		}() //多协程运行，
+		}()
 	}
 	return &p
 }
-func (p *WorkPool) Run(w Worker) { //向通道发送work，协程运行
+func (p *WorkPool) Run(w Worker) {
 	p.work <- w
 }
 func (p *WorkPool) Shutdown() {
-	close(p.work) //关闭通道，协程退出循环
-	p.wg.Wait()   //等待协程终止
+	close(p.work)
+	p.wg.Wait()
 }
