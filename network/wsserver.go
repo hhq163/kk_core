20c9f48cc13e37c6bde0de6a01af02e9a0f447e6
hhq163 <hhq163@gmail.com>
2020-08-13 15:39:42 +0800

    change wsserver.go

--- network/wsserver.go
+++ network/wsserver.go
@@ -24,7 +24,7 @@ type WSServer struct {
 	HTTPTimeout time.Duration
 	CertFile    string
 	KeyFile     string
-	NewAgent    func(*WSConn) Agent
+	NewAgent    func(*Conn) Agent
 	ln          net.Listener
 	Handler     *KKHandle
 }
